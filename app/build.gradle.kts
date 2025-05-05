a5a26e3e408263988e286e0cd27edf318f356d0c
vancoder1 <ivanzaporozhets25@gmail.com>
2025-05-05 10:16:53 -0600

    Refactor: Increment app version code
    - Updated the `versionCode` in `build.gradle.kts` from 1 to 2, indicating a new app release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = rootProject.extra["MethodicId"] as String
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
