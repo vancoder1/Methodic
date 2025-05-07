f89dc34762cd2bfe524778ab5a54224c3369f06c
vancoder1 <ivanzaporozhets25@gmail.com>
2025-05-07 17:16:29 -0600

    Refactor: Increment app version code and update version name
    - Updated the `versionCode` in `build.gradle.kts` from 4 to 5.
    - Updated the `versionName` in `build.gradle.kts` from "1.1.1" to "1.1.1.01".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = rootProject.extra["MethodicId"] as String
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.1.1"
+        versionCode = 5
+        versionName = "1.1.1.01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
